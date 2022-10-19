3df6f544599024e1cd00dc2589786c5e97cd39b2
marpapad <marypapadaki13@gmail.com>
2022-10-20 00:26:00 +0300

    minor change

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from pipeline import *
 import argparse
 
-fp = efoPage(0).fetch()
-total_pages = fp['pages']
 
-parser = argparse.ArgumentParser(description="Efo Pipeline Arguments")
-parser.add_argument("-hs", "--host", type=str, help="set postgres host, default: localhost", default='localhost')
-parser.add_argument("-p", "--port", type=int, help="set postgres port, default: 5432", default=5432)
-parser.add_argument("-db", "--db", type=str, help="set postgres database, default: efo", default='efo')
-parser.add_argument("-u", "--user", type=str, help="set postgres user, default: postgres", default='postgres')
-parser.add_argument("-P", "--password", type=str, help="set user password - required")
-parser.add_argument("-pg", "--till_page", type=int, help="set page to be loaded upper limit - default: last ava", default=total_pages-1)
-args = parser.parse_args()
+if __name__ == '__main__':
 
-config = vars(args)
+    fp = efoPage(0).fetch()
+    total_pages = fp['pages']
 
-print(config)
- 
-efoPipe(**config).run()
+    parser = argparse.ArgumentParser(description="Efo Pipeline Arguments")
+    parser.add_argument("-hs", "--host", type=str, help="set postgres host, default: localhost", default='localhost')
+    parser.add_argument("-p", "--port", type=int, help="set postgres port, default: 5432", default=5432)
+    parser.add_argument("-db", "--db", type=str, help="set postgres database, default: efo", default='efo')
+    parser.add_argument("-u", "--user", type=str, help="set postgres user, default: postgres", default='postgres')
+    parser.add_argument("-P", "--password", type=str, help="set user password - required")
+    parser.add_argument("-pg", "--till_page", type=int, help="set page to be loaded upper limit - default: last ava", default=total_pages-1)
+    args = parser.parse_args()
+
+    config = vars(args)
+    
+    efoPipe(**config).run()
 
